1463cd46161a4d4f0fed7eb35b10504e124d9e43
JEH <han880330@yahoo.co.jp>
2022-01-24 14:34:24 +0900

    環境構築、全体のルーティングを分析、既存のHeaderとFooterを本プロジェクトに追加、004-005

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>i am genzujian</div>
+  <div>
+      <Header></Header>
+    i am genzujian
+      <Footer></Footer>
+  </div>
 </template>
 
 <script>
+//引入
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
-
+ name: '',
+ components:{
+   Header,
+   Footer
+ }
 }
 </script>
 
